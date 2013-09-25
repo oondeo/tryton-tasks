8349e8ff93d50d4ad733132105ac837b8d3ad7ac
Guillem Barba <guillem@nan-tic.com>
2013-09-25 11:37:45 +0200

    Recover update config before clone, moved out the hg_* functions and remove use of hgapi on hg_pull and hg_update

--- scm.py
+++ scm.py
@@ -23,17 +23,20 @@ def wait_processes(processes, maximum=MAX_PROCESSES):
         i += 1
 
 
+def hg_clone(url, repo_path):
+    command = 'hg clone -q %s %s' % (url, repo_path)
+    try:
+        run(command)
+    except:
+        print "Error running " + t.bold(command)
+        raise
+    print "Repo " + t.bold(repo_path) + t.green(" Cloned")
+
+
 @task()
 def clone(config=None):
-
-    def hg_clone(url, repo_path):
-        command = 'hg clone -q %s %s' % (url, repo_path)
-        try:
-            run(command)
-        except:
-            print "Error running " + t.bold(command)
-            raise
-        print "Repo " + t.bold(repo_path) + t.green(" Cloned")
+    # Updates config repo to get new repos in config files
+    hg_pull('config', '.', True)
 
     Config = read_config_file(config)
     p = None
@@ -56,42 +59,42 @@ def clone(config=None):
     wait_processes(processes, 0)
 
 
+def hg_status(module, path, verbose):
+    repo_path = os.path.join(path, module)
+    if not os.path.exists(repo_path):
+        print t.red("Missing repositori:") + t.bold(repo_path)
+        return
+    repo = hgapi.Repo(repo_path)
+    st = repo.hg_status(empty=True)
+    if not st and verbose:
+        print t.bold_green('\[' + module + ']')
+        return
+    if not st and not verbose:
+        return
+
+    msg = [t.bold_red("\n[" + module + ']')]
+
+    if st.get('A'):
+        for file_name in st['A']:
+            msg.append(t.green('A ' + file_name))
+    if st.get('M'):
+        for file_name in st['M']:
+            msg.append(t.yellow('M ' + file_name))
+    if st.get('R'):
+        for file_name in st['R']:
+            msg.append(t.red('R ' + file_name))
+    if st.get('!'):
+        for file_name in st['!']:
+            msg.append(t.bold_red('! ' + file_name))
+    if st.get('?'):
+        for file_name in st['?']:
+            msg.append(t.blue('? ' + file_name))
+
+    print "\n".join(msg)
+
+
 @task
 def status(config=None, verbose=False):
-
-    def hg_status(module, path, verbose):
-        repo_path = os.path.join(path, module)
-        if not os.path.exists(repo_path):
-            print t.red("Missing repositori:") + t.bold(repo_path)
-            return
-        repo = hgapi.Repo(repo_path)
-        st = repo.hg_status(empty=True)
-        if not st and verbose:
-            print t.bold_green('\[' + module + ']')
-            return
-        if not st and not verbose:
-            return
-
-        msg = [t.bold_red("\n[" + module + ']')]
-
-        if st.get('A'):
-            for file_name in st['A']:
-                msg.append(t.green('A ' + file_name))
-        if st.get('M'):
-            for file_name in st['M']:
-                msg.append(t.yellow('M ' + file_name))
-        if st.get('R'):
-            for file_name in st['R']:
-                msg.append(t.red('R ' + file_name))
-        if st.get('!'):
-            for file_name in st['!']:
-                msg.append(t.bold_red('! ' + file_name))
-        if st.get('?'):
-            for file_name in st['?']:
-                msg.append(t.blue('? ' + file_name))
-
-        print "\n".join(msg)
-
     Config = read_config_file(config)
     processes = []
     p = None
@@ -109,70 +112,72 @@ def status(config=None, verbose=False):
     wait_processes(processes, 0)
 
 
+def hg_diff(module, path, verbose, rev1, rev2):
+    t = Terminal()
+    try:
+        msg = []
+        path_repo = os.path.join(path, module)
+        if not os.path.exists(path_repo):
+            print t.red("Missing repositori:") + t.bold(path_repo)
+            return
+
+        if not verbose:
+            result = run('cd %s;hg diff --stat' % path_repo, hide='stdout')
+            if result.stdout:
+                msg.append(t.bold(module + "\n"))
+                msg.append(result.stdout)
+                print "\n".join(msg)
+            return
+        repo = hgapi.Repo(path_repo)
+        msg = []
+        for diff in repo.hg_diff(rev1, rev2):
+            if diff:
+                d = diff['diff'].split('\n')
+                for line in d:
+                    if line and line[0] == '-':
+                        line = t.red + line + t.normal
+                    elif line and line[0] == '+':
+                        line = t.green + line + t.normal
+
+                    if line:
+                        msg.append(line)
+        if msg == []:
+            return
+        msg.insert(0, t.bold('\n[' + module + "]\n"))
+        print "\n".join(msg)
+    except:
+        msg.insert(0, t.bold('\n[' + module + "]\n"))
+        msg.append(str(sys.exc_info()[1]))
+        print "\n".join(msg)
+
+
 @task
 def diff(config=None, verbose=True, rev1='default', rev2=None):
-    def hg_diff(module, path, verbose):
-        t = Terminal()
-        try:
-            msg = []
-            path_repo = os.path.join(path, module)
-            if not os.path.exists(path_repo):
-                print t.red("Missing repositori:") + t.bold(path_repo)
-                return
-
-            if not verbose:
-                result = run('cd %s;hg diff --stat' % path_repo, hide='stdout')
-                if result.stdout:
-                    msg.append(t.bold(module + "\n"))
-                    msg.append(result.stdout)
-                    print "\n".join(msg)
-                return
-            repo = hgapi.Repo(path_repo)
-            msg = []
-            for diff in repo.hg_diff(rev1, rev2):
-                if diff:
-                    d = diff['diff'].split('\n')
-                    for line in d:
-                        if line and line[0] == '-':
-                            line = t.red + line + t.normal
-                        elif line and line[0] == '+':
-                            line = t.green + line + t.normal
-
-                        if line:
-                            msg.append(line)
-            if msg == []:
-                return
-            msg.insert(0, t.bold('\n[' + module + "]\n"))
-            print "\n".join(msg)
-        except:
-            msg.insert(0, t.bold('\n[' + module + "]\n"))
-            msg.append(str(sys.exc_info()[1]))
-            print "\n".join(msg)
-
     Config = read_config_file(config)
     processes = []
     for section in Config.sections():
         path = Config.get(section, 'path')
-        p = Process(target=hg_diff, args=(section, path, verbose))
+        p = Process(target=hg_diff, args=(section, path, verbose, rev1, rev2))
         p.start()
         processes.append(p)
         wait_processes(processes)
     wait_processes(processes, 0)
 
 
+def hg_summary(module, path, verbose):
+    path_repo = os.path.join(path, module)
+    if not os.path.exists(path_repo):
+        print t.red("Missing repositori:") + t.bold(path_repo)
+        return
+    repo = hgapi.Repo(path_repo)
+    cmd = ['summary', '--remote']
+    summary = repo.hg_command(*cmd)
+    print t.bold("= " + module + " =")
+    print summary
+
+
 @task
 def summary(config=None, verbose=False):
-    def hg_summary(module, path, verbose):
-        path_repo = os.path.join(path, module)
-        if not os.path.exists(path_repo):
-            print t.red("Missing repositori:") + t.bold(path_repo)
-            return
-        repo = hgapi.Repo(path_repo)
-        cmd = ['summary', '--remote']
-        summary = repo.hg_command(*cmd)
-        print t.bold("= " + module + " =")
-        print summary
-
     Config = read_config_file(config)
     processes = []
     for section in Config.sections():
@@ -189,25 +194,26 @@ def summary(config=None, verbose=False):
     wait_processes(processes, 0)
 
 
+def hg_ppush(module, path, verbose):
+    path_repo = os.path.join(path, module)
+    if not os.path.exists(path_repo):
+        print t.red("Missing repositori:") + t.bold(path_repo)
+        return
+    repo = hgapi.Repo(path_repo)
+    cmd = ['outgoing']
+    if verbose:
+        cmd.append('-v')
+    try:
+        out = repo.hg_command(*cmd)
+    except:
+        #TODO:catch correct exception
+        return
+    print t.bold("= " + module + " =")
+    print out
+
+
 @task
 def ppush(config=None, verbose=False):
-    def hg_ppush(module, path, verbose):
-        path_repo = os.path.join(path, module)
-        if not os.path.exists(path_repo):
-            print t.red("Missing repositori:") + t.bold(path_repo)
-            return
-        repo = hgapi.Repo(path_repo)
-        cmd = ['outgoing']
-        if verbose:
-            cmd.append('-v')
-        try:
-            out = repo.hg_command(*cmd)
-        except:
-            #TODO:catch correct exception
-            return
-        print t.bold("= " + module + " =")
-        print out
-
     Config = read_config_file(config)
     processes = []
     for section in Config.sections():
@@ -224,28 +230,35 @@ def ppush(config=None, verbose=False):
     wait_processes(processes, 0)
 
 
+def hg_pull(module, path, update):
+    path_repo = os.path.join(path, module)
+    if not os.path.exists(path_repo):
+        print t.red("Missing repositori:") + t.bold(path_repo)
+        return
+
+    cwd = os.getcwd()
+    os.chdir(path_repo)
+
+    cmd = ['hg', 'pull']
+    if update:
+        cmd.append('-u')
+    result = run(' '.join(cmd), warn=True, hide='both')
+
+    if not result.ok:
+        print t.red("= " + module + " = KO!")
+        print result.stderr
+        os.chdir(cwd)
+        return
+
+    if "no changes found" in result.stdout:
+        return
+    print t.bold("= " + module + " =")
+    print result.stdout
+    os.chdir(cwd)
+
+
 @task
 def pull(config=None, update=True):
-    def hg_pull(module, path, update):
-        path_repo = os.path.join(path, module)
-        if not os.path.exists(path_repo):
-            print t.red("Missing repositori:") + t.bold(path_repo)
-            return
-        repo = hgapi.Repo(path_repo)
-        cmd = ['pull']
-        if update:
-            cmd.append('-u')
-        try:
-            out = repo.hg_command(*cmd)
-        except:
-            #TODO:catch correct exception
-            print t.red("= " + module + " = KO!")
-            return
-        if "no changes found" in out:
-            return
-        print t.bold("= " + module + " =")
-        print out
-
     Config = read_config_file(config)
     processes = []
     p = None
@@ -263,29 +276,36 @@ def pull(config=None, update=True):
     wait_processes(processes, 0)
 
 
+def hg_update(module, path, clean):
+    path_repo = os.path.join(path, module)
+    if not os.path.exists(path_repo):
+        print t.red("Missing repositori:") + t.bold(path_repo)
+        return
+
+    cwd = os.getcwd()
+    os.chdir(path_repo)
+
+    cmd = ['hg', 'update']
+    if clean:
+        cmd.append('-C')
+    result = run(' '.join(cmd), warn=True, hide='both')
+
+    if not result.ok:
+        print t.red("= " + module + " = KO!")
+        print result.stderr
+        os.chdir(cwd)
+        return
+
+    if (u"0 files updated, 0 files merged, 0 files removed, 0 "
+            u"files unresolved\n") in result.stdout:
+        return
+    print t.bold("= " + module + " =")
+    print result.stdout
+    os.chdir(cwd)
+
+
 @task
 def update(config=None, clean=False):
-    def hg_update(module, path, update):
-        path_repo = os.path.join(path, module)
-        if not os.path.exists(path_repo):
-            print t.red("Missing repositori:") + t.bold(path_repo)
-            return
-        repo = hgapi.Repo(path_repo)
-        cmd = ['update']
-        if clean:
-            cmd.append('-C')
-        try:
-            out = repo.hg_command(*cmd)
-        except:
-            #TODO:catch correct exception
-            print t.red("= " + module + " = KO!")
-            return
-        if (u"0 files updated, 0 files merged, 0 files removed, 0 "
-                u"files unresolved\n") in out:
-            return
-        print t.bold("= " + module + " =")
-        print out
-
     Config = read_config_file(config)
     processes = []
     p = None
@@ -296,7 +316,7 @@ def update(config=None, clean=False):
         if repo != 'hg':
             print "Not developet yet"
             continue
-        p = Process(target=func, args=(section, path, update))
+        p = Process(target=func, args=(section, path, clean))
         p.start()
         processes.append(p)
         wait_processes(processes)
