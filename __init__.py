5ae296139e8a4bff5b19234e5f7a59810d6b4d9b
Sergi Almacellas Abellana <sergi@koolpi.com>
2014-03-07 16:37:23 +0100

    Add new branch task to change branch of all existing repositories

---
+++ __init__.py
@@ -0,0 +1,26 @@
+import bootstrap
+import utils
+from .scm import *
+import patch
+#import nantic
+import tryton
+from invoke import Collection
+
+
+ns = Collection()
+ns.add_task(clone)
+ns.add_task(status)
+ns.add_task(resolve)
+ns.add_task(diff)
+ns.add_task(summary)
+ns.add_task(outgoing)
+ns.add_task(pull)
+ns.add_task(update)
+ns.add_task(repo_list)
+ns.add_task(fetch)
+ns.add_task(branch)
+ns.add_collection(Collection.from_module(bootstrap), 'bs')
+ns.add_collection(Collection.from_module(utils))
+ns.add_collection(Collection.from_module(patch))
+#ns.add_collection(Collection.from_module(nantic))
+ns.add_collection(Collection.from_module(tryton))
