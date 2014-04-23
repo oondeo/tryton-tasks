ab0f47f58637e8a99905f669c0e488ea9170af56
Albert Cervera i Areny <albert@nan-tic.com>
2014-04-23 10:01:34 +0200

    Improve gal to import data.

--- gal.py
+++ gal.py
@@ -4,6 +4,8 @@ import sys
 import time
 import subprocess
 import hgapi
+import random
+from decimal import Decimal
 from invoke import task
 
 from .utils import t
@@ -22,10 +24,12 @@ from proteus import config as pconfig, Model, Wizard
 
 
 def check_output(*args):
+    t.bold(' '.join(args))
     process = subprocess.Popen(args, stdout=subprocess.PIPE,
         stderr=subprocess.PIPE)
     process.wait()
     data = process.stdout.read()
+    data += process.stderr.read()
     return data
 
 def connect_database(database=None, password='admin',
@@ -35,6 +39,11 @@ def connect_database(database=None, password='admin',
     global config 
     config = pconfig.set_trytond(database, database_type=database_type,
         password=password, config_file='trytond.conf')
+
+def database_name():
+    import uuid
+    return uuid.uuid4()
+
 
 def dump(dbname=None):
     if dbname is None:
@@ -52,8 +61,9 @@ def restore(dbname=None):
     if dbname is None:
         dbname = 'gal'
     dump_file = 'gal.sql'
-    check_output('dropdb', dbname)
-    check_output('psql', '-f', gal_path(dump_file), dbname)
+    print check_output('dropdb', dbname)
+    print check_output('createdb', dbname)
+    print check_output('psql', '-f', gal_path(dump_file), dbname)
 
 def gal_path(path=None):
     res = 'gal'
@@ -201,3 +211,162 @@ def install_modules(modules):
 
     gal_commit()
     return modules, installed_modules
+
+def create_party(name, street=None, zip=None, city=None,
+        subdivision_code=None, country_code='ES', phone=None, website=None,
+        address_name=None,
+        account_payable=None, account_receivable=None):
+    Address = Model.get('party.address')
+    ContactMechanism = Model.get('party.contact_mechanism')
+    Country = Model.get('country.country')
+    Party = Model.get('party.party')
+    Subdivision = Model.get('country.subdivision')
+
+    parties = Party.find([('name', '=', name)])
+    if parties:
+        return parties[0]
+
+    country, = Country.find([('code', '=', country_code)])
+    if subdivision_code:
+        subdivision, = Subdivision.find([('code', '=', subdivision_code)])
+    else:
+        subdivision = None
+
+    party = Party(name=name)
+    party.addresses.pop()
+    party.addresses.append(
+        Address(
+            name=address_name,
+            street=street,
+            zip=zip,
+            city=city,
+            country=country,
+            subdivision=subdivision))
+    if phone:
+        party.contact_mechanisms.append(
+            ContactMechanism(type='phone',
+                value=phone))
+    if website:
+        party.contact_mechanisms.append(
+            ContactMechanism(type='website',
+                value=website))
+
+    if account_payable:
+        party.account_payable = account_payable
+    if account_receivable:
+        party.account_receivable = account_receivable
+
+    party.save()
+    return party
+
+@task
+def create_parties():
+    gal_action('create_parties')
+    restore()
+    connect_database()
+
+    with open('tasks/companies.txt', 'r') as f:
+        companies = f.read().split('\n')
+    with open('tasks/streets.txt', 'r') as f:
+        streets = f.read().split('\n')
+    with open('tasks/names.txt', 'r') as f:
+        names = f.read().split('\n')
+    with open('tasks/surnames.txt', 'r') as f:
+        surnames = f.read().split('\n')
+    phones = ['93', '972', '973', '977', '6', '900']
+    for x in xrange(4000):
+        company = random.choice(companies)
+        name = random.choice(names)
+        surname1 = random.choice(surnames)
+        surname2 = random.choice(surnames)
+        street = random.choice(streets)
+        name = '%s %s, %s' % (surname1, surname2, name)
+        street = '%s, %d' % (street, random.randrange(1, 100))
+        phone = random.choice(phones)
+        while len(phone) < 9:
+            phone += str(random.randrange(9))
+        create_party(company, street=street, zip=None, city=None,
+            subdivision_code=None, country_code='ES', phone=phone,
+            website=None, address_name=name)
+
+    gal_commit()
+
+def create_product(name, code="", template=None, cost_price=None,
+        list_price=None, type='goods', unit=None, consumable=False):
+
+    ProductUom = Model.get('product.uom')
+    Product = Model.get('product.product')
+    ProductTemplate = Model.get('product.template')
+    Account = Model.get('account.account')
+    Company = Model.get('company.company')
+    company = Company(1)
+
+    product = Product.find([('name', '=', name), ('code', '=', code)])
+    if product:
+        return product[0]
+
+    if not cost_price:
+        cost_price = random.randrange(0, 1000)
+
+    if not list_price:
+        list_price = cost_price * random.randrange(1, 2)
+
+    if unit is None:
+        unit = ProductUom(1)
+
+    if template is None:
+        template = ProductTemplate()
+        template.name = name
+        template.default_uom = unit
+        template.type = type
+        template.consumable = consumable
+        template.list_price = Decimal(str(list_price))
+        template.cost_price = Decimal(str(cost_price))
+
+        if hasattr(template, 'account_expense'):
+            expense = Account.find([
+                ('kind', '=', 'expense'),
+                ('company', '=', company.id),
+                ])
+            if expense:
+                template.account_expense = expense[0]
+        if hasattr(template, 'account_revenue'):
+            revenue = Account.find([
+                ('kind', '=', 'revenue'),
+                ('company', '=', company.id),
+                ])
+            if revenue:
+                template.account_revenue = revenue[0]
+
+        template.products[0].code = code
+        template.save()
+        product = template.products[0]
+    else:
+        product = Product()
+        product.template = template
+        product.code = code
+        product.save()
+    return product
+
+@task
+def create_products():
+    gal_action('create_parties')
+    restore()
+    connect_database()
+
+    import xmltodict
+    with open('tasks/catalog.xml', 'r') as f:
+        xml = xmltodict.parse(f.read())
+    count = 0
+    for item in xml.get('ICECAT-interface').get('files.index').get('file'):
+        name = item.get('@Model_Name')
+        create_product(name)
+        count += 1
+        if count >= 400:
+            break
+
+    gal_commit()
+
+
+# TODO: Add get and set functions to allow dumping a database with a given name
+# and adding to the repository a snapshot of a given database
