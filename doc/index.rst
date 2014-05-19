1963adf4594d058547b610860a3e6db378dd7f42
Albert Cervera i Areny <albert@nan-tic.com>
2014-05-20 00:59:47 +0200

    Fix bug in project.py

---
+++ doc/index.rst
@@ -0,0 +1,11 @@
+-------------------
+Tryton Invoke Tasks
+-------------------
+
+This package provide a sort of Invoke's task collections to develop on Tryton
+when you get the sources directly from VCS repositories.
+
+.. toctree::
+   :maxdepth: 2
+
+   gal
---
+++ project.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+import os
+import sys
+from invoke import task
+from .config import get_config
+import reviewboard
+from scm import get_branch
+
+try:
+    from proteus import config as pconfig, Model
+except ImportError, e:
+    print >> sys.stderr, "trytond importation error: ", e
+
+os.environ['TZ'] = "Europe/Madrid"
+settings = get_config()
+
+
+def get_tryton_connection():
+    tryton = settings['tryton']
+    return pconfig.set_xmlrpc(tryton['server'])
+
+
+@task
+def list(party=None, user=None):
+    get_tryton_connection()
+
+    Project = Model.get('project.work')
+    domain = [('state', '=', 'opened')]
+
+    if party:
+        domain.append(('party.name', 'like', party))
+    if user:
+        domain.append(('assigned_employees', 'like', user))
+
+    projects = Project.find(domain)
+    for project in projects:
+        print "(%s) %s - (%s) %s [%s]" %(
+            project.assigned_employees,
+            project.rec_name,
+            project.effort,
+            project.task_phase.name,
+            project.party.rec_name)
+
+
+@task
+def close_review(task):
+    get_tryton_connection()
+    Review = Model.get('project.work.codereview')
+    reviews = Review.find([('work.code', '=', task)])
+    for review in reviews:
+        reviewboard.close(review.review_id)
+
+@task
+def fetch_review(task):
+    get_tryton_connection()
+    Review = Model.get('project.work.codereview')
+    reviews = Review.find([('work.code', '=', task)])
+    for review in reviews:
+        if review.component:
+           path = os.path.join('modules', review.component.name)
+        else:
+            path = ''
+        reviewboard.fetch(path, review.review_id)
+
+
+@task
+def upload_review(task, path, review=None):
+    get_tryton_connection()
+    Review = Model.get('project.work.codereview')
+    Task = Model.get('project.work')
+    Component = Model.get('project.work.component')
+    module = path.split('/')[-1]
+    component = Component.find([('name', '=', module)])
+    tasks = Task.find( [('code', '=', task)])
+    t = tasks and tasks[0]
+    review_id = reviewboard.create(path, t.rec_name,
+        t.comment, t.code, review)
+
+    review = Review.find([('id', '=', str(review_id))])
+    if not review:
+        review = Review()
+
+    review.name = t.rec_name
+    review.review_id = str(review_id)
+    review.url = 'http://git.nan-tic.com/reviews/r/%s'%review_id
+    review.work = t
+    review.branch = get_branch(path)
+    review.component = component and component[0]
+    review.save()
