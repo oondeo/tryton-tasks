bf65e5632a4d1dad32031a96694ed1bcc60509d2
Guillem Barba <guillem@nan-tic.com>
2014-07-09 16:15:16 +0200

    Add update_all() task

---
+++ __init__.py
@@ -0,0 +1,52 @@
+import bootstrap
+import utils
+from .scm import *
+import patch
+import tryton
+from invoke import Collection
+import reviewboard
+import tests
+import config
+import gal
+import tryton_component
+import project
+import bbucket
+import userdoc
+
+ns = Collection()
+ns.add_task(clone)
+ns.add_task(status)
+ns.add_task(resolve)
+ns.add_task(diff)
+ns.add_task(summary)
+ns.add_task(outgoing)
+ns.add_task(push)
+ns.add_task(pull)
+ns.add_task(update)
+ns.add_task(repo_list)
+ns.add_task(fetch)
+ns.add_task(unknown)
+ns.add_task(stat)
+ns.add_task(branch)
+ns.add_task(missing_branch)
+ns.add_task(create_branch)
+ns.add_task(compare_branches)
+ns.add_task(module_diff)
+ns.add_task(add2virtualenv)
+ns.add_task(increase_version)
+ns.add_task(revision)
+ns.add_task(clean)
+ns.add_collection(Collection.from_module(bootstrap), 'bs')
+ns.add_collection(Collection.from_module(utils))
+ns.add_collection(Collection.from_module(patch))
+ns.add_collection(Collection.from_module(tryton))
+ns.add_collection(Collection.from_module(tests))
+ns.add_collection(Collection.from_module(reviewboard), 'rb')
+ns.add_collection(Collection.from_module(config))
+ns.add_collection(Collection.from_module(gal))
+ns.add_collection(Collection.from_module(tryton_component), 'component')
+ns.add_collection(Collection.from_module(project), 'project')
+ns.add_collection(Collection.from_module(bbucket), 'bb')
+ns.add_collection(Collection.from_module(userdoc), 'doc')
+
+
---
+++ config.py
@@ -0,0 +1,87 @@
+import os
+import ConfigParser
+from invoke import task, run
+from .utils import read_config_file, get_config_files
+from .scm import get_repo
+from collections import OrderedDict
+
+
+def get_config():
+    """ Get config file for tasks module """
+    parser = ConfigParser.ConfigParser()
+    config_path = '%s/.tryton-tasks.cfg' % os.getenv('HOME')
+    parser.read(config_path)
+    settings = {}
+    for section in parser.sections():
+        usection = unicode(section, 'utf-8')
+        settings[usection] = {}
+        for name, value, in parser.items(section):
+            settings[usection][name] = value
+    return settings
+
+
+@task
+def set_revision(config=None):
+    """ Set branch on repository config files """
+
+    if config is None:
+        config_files = get_config_files()
+    else:
+        config_files = [config]
+
+    for config_file in config_files:
+        Config = read_config_file(config_file, type='all', unstable=True)
+        f_d = open(config_file, 'w+')
+        for section in Config.sections():
+            if Config.has_option(section, 'patch'):
+                continue
+            repo = get_repo(section, Config, 'revision')
+            revision = repo['function'](section, repo['path'], verbose=False)
+            Config.set(section, 'revision', revision)
+
+        Config.write(f_d)
+        f_d.close()
+
+
+@task
+def set_branch(branch, config=None):
+    """ Set branch on repository config files """
+
+    if config is None:
+        config_files = get_config_files()
+    else:
+        config_files = [config]
+
+    for config_file in config_files:
+        Config = read_config_file(config_file, type='all', unstable=True)
+        f_d = open(config_file, 'w+')
+        for section in Config.sections():
+            if Config.has_option(section, 'patch'):
+                continue
+            Config.set(section, 'branch', branch)
+
+        Config.write(f_d)
+        f_d.close()
+
+
+@task
+def add_module(config, path, url=None):
+    """ Add module to specified config file """
+    Config = read_config_file(config, type='all', unstable=True)
+    module = os.path.basename(path)
+    url = run('cd %s; hg paths default' % (path)).stdout.split('\n')[0]
+    if 'http' in url:
+        url = 'ssh://hg@bitbucket.org/nantic/trytond-%s' % module
+    branch = run('cd %s;hg branch' % (path)).stdout.split('\n')[0]
+    cfile = open(config, 'w+')
+    if not Config.has_section(module):
+        Config.add_section(module)
+        Config.set(module, 'branch', branch)
+        Config.set(module, 'repo', 'hg')
+        Config.set(module, 'url', url)
+        Config.set(module, 'path', './trytond/trytond/modules')
+
+    Config._sections = OrderedDict(sorted(Config._sections.iteritems(),
+        key=lambda x: x[0]))
+    Config.write(cfile)
+    cfile.close()
