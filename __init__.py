ab75b1ee8950876a7815f5d59f2f6c6dbdc05e5e
Guillem Barba <guillem@nan-tic.com>
2014-07-24 09:43:09 +0200

    userdoc: add treatment for builder 'pdf'

---
+++ __init__.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from .scm import ScmCollection as ns
+from .bootstrap import BootstrapCollection
+from .config import ConfigCollection
+from .bbucket import BitbucketCollection
+from .utils import UtilsCollection
+from .tryton import TrytonCollection
+from .tests import TestCollection
+from .reviewboard import ReviewCollection
+from .tryton_component import ComponentCollection
+from .project import ProjectCollection
+from .userdoc import DocCollection
+from .quilt import QuiltCollection
+from .gal import GalCollection
+
+ns.add_collection(BootstrapCollection, 'bs')
+ns.add_collection(BitbucketCollection, 'bb')
+ns.add_collection(UtilsCollection, 'utils')
+ns.add_collection(ConfigCollection, 'config')
+ns.add_collection(TrytonCollection, 'tryton')
+ns.add_collection(TestCollection, 'test')
+ns.add_collection(ReviewCollection, 'rb')
+ns.add_collection(ComponentCollection, 'component')
+ns.add_collection(ProjectCollection, 'project')
+ns.add_collection(DocCollection, 'doc')
+ns.add_collection(QuiltCollection, 'quilt')
+ns.add_collection(GalCollection, 'gal')
---
+++ tests.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+from invoke import task, run, Collection
+import scm
+import utils
+import project
+
+# from .scm import prefetch, fetch, get_repo, remove_dir,
+#      hg_clone, git_clone
+# from .utils import read_config_file
+
+
+@task()
+def test(output=None, coverage=False, flakes=False,
+        fail_fast=True, dbtype='sqlite', mail=False, module=None):
+
+    cmd = ['python','test.py']
+    if output:
+        cmd += ['--output', output]
+
+    if coverage:
+        cmd.append('--coverage')
+    if flakes:
+        cmd.append('--flakes')
+    if fail_fast:
+        cmd.append('--fail-fast')
+    cmd.append('--db-type %s' % dbtype)
+    if mail:
+        cmd.append('--mail')
+
+    run(" ".join(cmd), echo=True)
+
+
+@task
+def runall(test_file, output, branch='default'):
+    print "Setup & testing stable revision of branch: %s " % branch
+    setup(branch, development=False)
+    runtests(test_file, output, branch, development=False,
+        include_reviews=False)
+    runtests(test_file, output, branch, development=False, include_reviews=True)
+    print "Setup & testing development revision of branch: %s " % branch
+    setup(branch, development=True)
+    runtests(test_file, output, branch, development=True, include_reviews=False)
+    runtests(test_file, output, branch, development=True, include_reviews=True)
+
+
+@task
+def runtests(test_file=None, output=None, branch='default', development=False,
+        include_reviews=False):
+
+    sections = []
+    if test_file:
+        config = utils.read_config_file(test_file)
+        sections = config.sections()
+
+    coverage = True
+    flakes = True
+    fail_fast = True
+    mail = True
+
+    if include_reviews:
+        project.fetch_reviews(branch, exclude_components=config.sections())
+
+    test(output, False, False, fail_fast, 'sqlite', mail)
+    test(output, coverage, flakes, fail_fast, 'postgresql', mail)
+
+    for section in sections:
+        repo = utils.get_repo(section, config, 'clone', development)
+        if repo['branch'] != branch:
+            continue
+        func = eval('scm.%s', repo['function'])
+        func(repo['url'], repo['path'], repo['branch'], repo['revision'])
+        if include_reviews:
+            project.fetch_reviews(component=section)
+        test(output, False, False, fail_fast, 'sqlite', mail, section)
+        test(output, coverage, flakes, fail_fast, 'postgresql', mail, section)
+        utils.remove_dir(repo['path'], quiet=True)
+
+    clean()
+
+
+@task()
+def clean(force=True):
+    scm.prefetch(force=force)
+
+
+@task()
+def setup(branch='default', development=False, force=True):
+    clean(force=force)
+    scm.branch(branch, clean=force)
+    clean(force=force)
+    scm.fetch()
+
+
+TestCollection = Collection()
+TestCollection.add_task(test)
+TestCollection.add_task(clean)
+TestCollection.add_task(setup)
+TestCollection.add_task(runtests)
+TestCollection.add_task(runall)
+
+
