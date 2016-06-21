916cb836acb9ef209ed137489eb3a031b22bbcd4
Raimon Esteve <raimon@nan-tic.com>
2016-06-21 15:38:59 +0200

    Version invoke==0.12.2

---
+++ bootstrap.py
@@ -0,0 +1,244 @@
+#!/usr/bin/env python
+
+import ConfigParser
+import os
+from blessings import Terminal
+from invoke import Collection, task, run
+from path import path
+
+from .utils import _ask_ok, _check_required_file, _exit
+from .scm import hg_clone, hg_pull, clone, fetch
+
+
+t = Terminal()
+Config = ConfigParser.ConfigParser()
+
+# TODO: l'us que faig del config potser correspon a context
+# http://docs.pyinvoke.org/en/latest/getting_started.html#handling-configuration-state
+
+INITIAL_PATH = path.getcwd()
+
+
+@task()
+def get_tasks(taskpath='tasks'):
+    # TODO: add option to update repository
+    Config.tasks_path = taskpath
+    if path(taskpath).exists():
+        print 'Updating tasks repo'
+        hg_pull(taskpath, '.', True)
+        return
+
+    if not getattr(Config, 'get_tasks', False):
+        if not _ask_ok('Are you in the customer project directory? '
+                'Answer "yes" to clone the "tryton-tasks" repository '
+                'in "%s" directory. [Y/n] ' % taskpath, 'y'):
+            return
+
+    print ('Cloning ssh://hg@bitbucket.org/nantic/tryton-tasks '
+        'repository in "tasks" directory.')
+    hg_clone('ssh://hg@bitbucket.org/nantic/tryton-tasks', taskpath)
+    print ""
+
+
+@task()
+def get_config(configpath='config', branch='default'):
+    # TODO: add option to update repository
+    Config.config_path = path(configpath).abspath()
+    if path(configpath).exists():
+        print ('Updating config repo')
+        hg_pull(configpath, '.', True, branch=branch)
+        return
+
+    if not getattr(Config, 'get_config', False):
+        if not _ask_ok('Are you in the customer project directory? '
+                'Answer "yes" to clone the "tryton-config" repository '
+                'in "%s" directory. [Y/n] ' % configpath, 'y'):
+            return
+
+    print ('Cloning ssh://hg@bitbucket.org/nantic/tryton-config '
+        'repository in "config" directory.')
+    hg_clone('ssh://hg@bitbucket.org/nantic/tryton-config', configpath, branch)
+    print ""
+
+
+@task()
+def get_utils(utilspath='utils'):
+    # TODO: add option to update repository
+    Config.utils_path = utilspath
+    if path(utilspath).exists():
+        print 'Updating utils repo'
+        hg_pull(utilspath, '.', True)
+        return
+
+    if not getattr(Config, 'get_utils'):
+        if not _ask_ok('Are you in the customer project directory? '
+                'Answer "yes" to clone the "nan_tryton_utils" repository '
+                'in "%s" directory. [Y/n] ' % utilspath, 'y'):
+            return
+
+    print ('Cloning ssh://hg@bitbucket.org/nantic/nan_tryton_utils '
+        'repository in "utils" directory.')
+    hg_clone('ssh://hg@bitbucket.org/nantic/nan_tryton_utils', utilspath)
+    print ""
+
+
+@task()
+def activate_virtualenv(projectname):
+    '''
+    Config.virtualenv indicates virtualenv must to be activated
+    Config.virtualenv_active informs virtualenv is activated
+
+    To ensure you doesn't forgotten to activate virtualenv,
+        if not Config.virtualenv but environment variable 'VIRTUAL_ENV' exists,
+        it asks you if you want to activate it.
+    '''
+    if os.environ.get('VIRTUAL_ENV'):
+        # Virtualenv already activated
+        Config.virtualenv = True
+        Config.virtualenv_active = True
+        return
+
+    if not Config.virtualenv and 'WORKON_HOME' in os.environ:
+        # virtualenvwrapper avilable. confirm don't activate virtualenv
+        if _ask_ok('You have available the "virtualenvwrapper". Are you '
+                'sure you don\'t whant to prepare project in a virtualenv? '
+                'Answer "yes" to continue without activate a virtualenv. '
+                '[Yes/no (activate)] ', 'y'):
+            Config.virtualenv_active = False
+            return
+        Config.virtualenv = True
+
+    if not Config.virtualenv:
+        Config.virtualenv_active = False
+        return
+
+    if 'WORKON_HOME' in os.environ:
+        virtualenv_path = path(os.environ['WORKON_HOME']).joinpath(
+            projectname)
+        if virtualenv_path.exists() and virtualenv_path.isdir():
+            activate_this_path = virtualenv_path.joinpath('bin/activate_this.py')
+            print "Activating virtualenv %s" % projectname
+            run(activate_this_path)
+
+
+@task(['get_config', 'activate_virtualenv'])
+def install_requirements(upgrade=False):
+    if not Config.requirements:
+        return
+    if not hasattr(Config, 'virtualenv_active') and os.geteuid() != 0:
+        resp = raw_input('It can\'t install requirements because you aren\'t '
+            'the Root user and you aren\'t in a Virtualenv. You will have to '
+            'install requirements manually as root with command:\n'
+            '  $ pip install [--upgrade] -r requirements.txt\n'
+            'What do you want to do now: skip requirements install or abort '
+            'bootstrap? [Skip/abort] ')
+        if resp.lower() not in ('', 's', 'skip', 'a', 'abort'):
+            _exit(INITIAL_PATH, 'Invalid answer.')
+        if resp.lower() in ('a', 'abort'):
+            _exit(INITIAL_PATH)
+        if resp.lower() in ('', 's', 'skip'):
+            return
+
+    print 'Installing dependencies.'
+    _check_required_file('requirements.txt', Config.config_path.basename(),
+        Config.config_path)
+    if upgrade:
+        run('pip install --upgrade -r %s/requirements.txt'
+            % Config.config_path)
+        #    _out=options.output, _err=sys.stderr)
+    else:
+        run('pip install -r %s/requirements.txt' % Config.config_path)
+        #    _out=options.output, _err=sys.stderr)
+    print ""
+
+
+# TODO: prepare_local() => set configuration options for future bootstrap based
+# on Config values
+
+
+@task()
+def install_proteus(proteuspath=None, upgrade=False):
+    print "Installing proteus."
+    if proteuspath is None:
+        cmd = ['pip', 'install', 'proteus']
+        if upgrade:
+            cmd.insert(2, '-u')
+        run(' '.join(cmd))
+    else:
+        if not path(proteuspath).exists():
+            _exit(INITIAL_PATH, "ERROR: Proteus path '%s' doesn't exists."
+                % proteuspath)
+        cwd = path.getcwd()
+        os.chdir(proteuspath)
+        run('python setup.py install')
+        os.chdir(cwd)
+    print ""
+
+
+@task()
+def create_symlinks():
+    cwd = path.getcwd()
+    if not os.path.isfile(os.path.join(cwd, 'utils', 'script-symlinks.sh')):
+        print 'Symlinks script not found'
+        return
+    os.chdir(os.path.join(cwd, 'utils'))
+    run('./script-symlinks.sh', warn=True)
+    os.chdir(cwd)
+
+
+@task(default=True)
+def bootstrap(branch, projectpath='', projectname='',
+        taskspath='tasks',
+        configpath='config',
+        utilspath='utils',
+        virtualenv=True,
+        upgradereqs=False):
+
+    if projectpath:
+        projectpath = path(projectpath)
+        os.chdir(projectpath)
+    elif INITIAL_PATH.basename() == 'tasks':
+        projectpath = INITIAL_PATH.parent()
+        os.chdir(projectpath)
+    else:
+        projectpath = INITIAL_PATH
+
+    if not projectname:
+        projectname = str(projectpath.basename())
+    Config.project_name = projectname
+
+    Config.virtualenv = virtualenv
+
+    # TODO: parse local.cfg to Config if exists?
+    Config.get_tasks = True
+    Config.get_config = True
+    Config.get_utils = True
+    Config.requirements = True  # Install?
+
+    get_tasks(taskspath)
+    get_config(configpath, branch=branch)
+    get_utils(utilspath)
+    activate_virtualenv(projectname)
+    install_requirements(upgrade=upgradereqs)
+
+    clone('config/base.cfg')
+    fetch()
+
+    create_symlinks()
+
+    if path.getcwd() != INITIAL_PATH:
+        os.chdir(INITIAL_PATH)
+
+
+__all__ = ['get_tasks', 'get_config', 'get_utils', 'activate_virtualenv',
+    'install_requirements', 'install_proteus', 'create_symlinks', 'bootstrap']
+
+BootstrapCollection = Collection()
+BootstrapCollection.add_task(bootstrap)
+BootstrapCollection.add_task(get_config)
+BootstrapCollection.add_task(get_tasks)
+BootstrapCollection.add_task(get_utils)
+BootstrapCollection.add_task(activate_virtualenv)
+BootstrapCollection.add_task(install_requirements)
+BootstrapCollection.add_task(install_proteus)
+BootstrapCollection.add_task(create_symlinks)
---
+++ tests.py
@@ -0,0 +1,271 @@
+#!/usr/bin/env python
+from invoke import task, run, Collection
+import os
+import subprocess
+import sys
+import tempfile
+import scm
+import utils
+import project
+from ConfigParser import NoOptionError
+import logging
+import time
+from coverage import coverage
+from .utils import read_config_file
+import tryton_component as component
+
+TEST_FILE = 'tests.log'
+open(TEST_FILE, 'w').close()
+
+logging.basicConfig(filename=TEST_FILE, level=logging.INFO,
+    format='[%(asctime)s] %(name)s: %(message)s',
+    datefmt='%m/%d/%Y %I:%M:%S %p')
+logger = logging.getLogger("nan-tasks")
+
+# Ensure path is loaded correctly
+sys.path.insert(0, os.path.abspath(os.path.normpath(os.path.join(
+        os.path.dirname(__file__), '..', ''))))
+for module_name in ('trytond', 'proteus'):
+    DIR = os.path.abspath(os.path.normpath(os.path.join(
+                os.path.dirname(__file__), '..', module_name)))
+    if os.path.isdir(DIR):
+        sys.path.insert(0, DIR)
+
+# Now we should be able to import everything
+import TrytonTestRunner
+
+older_version = True
+try:
+    # TODO: Remove compatibility with older versions
+    from trytond.config import CONFIG
+except ImportError:
+    try:
+        from trytond.config import config as CONFIG
+        older_version = False
+    except:
+        pass
+
+def check_output(*args):
+    process = subprocess.Popen(args, stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE)
+    process.wait()
+    data = process.stdout.read()
+    return data
+
+def get_fqdn():
+    data = check_output('hostname','--fqdn')
+    data = data.strip('\n').strip('\r').strip()
+    if not data:
+        # In some hosts we may get an error message on stderr with
+        # 'No such host'.
+        # if using --fqdn parameter. In this case, try to run hostname
+        # without parameters.
+        data = check_output('hostname')
+        data = data.strip('\n').strip('\r').strip()
+    return data
+
+def test(dbtype, name, modules, failfast, reviews=False, work=None,
+    upload=True):
+
+    if older_version:
+        CONFIG['db_type'] = dbtype
+        if not CONFIG['admin_passwd']:
+            CONFIG['admin_passwd'] = 'admin'
+    elif dbtype != 'sqlite':
+        CONFIG.set('database', 'uri', 'postgresql:///')
+
+    if dbtype == 'sqlite':
+        database_name = ':memory:'
+    else:
+        database_name = 'test_' + str(int(time.time()))
+
+    if name is None:
+        name = ''
+
+    os.environ['DB_NAME'] = database_name
+    cov = coverage()
+    cov.start()
+    from trytond.tests.test_tryton import modules_suite
+    import proteus.tests
+
+    suite = None
+    if modules:
+        suite = modules_suite(modules)
+    else:
+        suite = modules_suite()
+        suite.addTests(proteus.tests.test_suite())
+
+    runner = TrytonTestRunner.TrytonTestRunner(failfast=failfast, coverage=cov)
+    runner.run(suite)
+    if modules:
+        name = name + " ["+','.join(modules)+"]"
+
+    logger.info('Upload results to tryton')
+    if upload:
+        runner.upload_tryton(dbtype, failfast, name, reviews, work)
+    else:
+        runner.print_report(dbtype, failfast, name, reviews, work)
+
+@task()
+def module(module, work=None,  dbtype='sqlite', fail_fast=False, upload=True):
+    name = 'Development Test on "%s" for module' % get_fqdn()
+    test(failfast=fail_fast, dbtype=dbtype, modules=[module], name=name,
+        work=work, upload=upload)
+
+
+@task()
+def modules(dbtype='sqlite', force=False):
+    Config = read_config_file()
+
+    components = {}
+    if not force:
+        components = component._pull()
+
+    to_test = []
+
+    for section in Config.sections():
+        comp = components.get(section, False)
+
+        if force or not comp:
+            to_test.append(section)
+            continue
+
+        test_revision = comp.last_build and comp.last_build.revision
+        repo = scm.get_repo(section, Config, 'revision')
+        if (test_revision or (test_revision and
+                scm.hg_is_last_revision(repo['path'], test_revision))):
+            continue
+        to_test.append(section)
+
+    print "Testing :", len(to_test), "/", len(Config.sections())
+    for tm in to_test:
+        try:
+            print "Testing module:", tm
+            module(tm)
+            #run('inv test.module -m %s' % tm)
+        except:
+            logger.exception("Exception has occured", exc_info=1)
+
+
+
+@task()
+def runall(test_file, dbtype='sqlite', branch='default', exclude_reviews=False,
+        fail_fast=False):
+
+    try:
+        logger.info('Setting to branch: %s', branch)
+        setup(branch)
+        logger.info('Testing Branch %s with:'
+            ' Include reviews: %s'
+            ' Database Type: %s'
+            ' Fail Fast: %s' % (branch, False, dbtype, fail_fast))
+        runtests(test_file, branch, include_reviews=False,
+            dbtype=dbtype, fail_fast=fail_fast)
+        if not exclude_reviews:
+            logger.info('Testing Branch %s with Reviews:'
+            ' Include reviews: %s'
+            ' Database Type: %s'
+            ' Fail Fast: %s' % (branch, True, dbtype, fail_fast))
+            runtests(test_file, branch, development=False,
+                include_reviews=True, dbtype=dbtype, fail_fast=fail_fast)
+    except:
+        logger.exception("Exception has occured", exc_info=1)
+        project.create_test_task(TEST_FILE)
+
+
+def runtests(test_file=None, branch='default', development=False,
+        include_reviews=False, dbtype='sqlite', fail_fast=False):
+
+    directory = tempfile.mkdtemp()
+    run("cp . %s -R" % directory)
+    old_dir = os.getcwd()
+    os.chdir(directory)
+    try:
+        setup(branch, development, fetch=False)
+    except:
+        project.create_test_task(TEST_FILE)
+        logger.exception("Exception has occured", exc_info=1)
+        return
+
+    sections = []
+    if test_file:
+        config = utils.read_config_file(test_file)
+        sections = config.sections()
+
+    if test_file and include_reviews:
+        try:
+            logger.info('Fetching reviews')
+            project._fetch_reviews(branch,
+                exclude_components=config.sections() + ['OpenERP'])
+        except:
+            logger.exception("Exception has occured", exc_info=1)
+            os.chdir(old_dir)
+            run("rm -Rf %s" % directory)
+            project.create_test_task(TEST_FILE)
+            return
+
+    name = 'Generic Modules'
+    if development:
+            name = '%s - Development' % name
+    if include_reviews:
+        name = '%s with reviews' % name
+
+    logger.info('%s Testing...' % name)
+
+    test(failfast=fail_fast, dbtype=dbtype, reviews=include_reviews, name=name,
+        modules=None)
+
+    for section in sections:
+
+        name2 = name + "/" + section
+        repos_to_clone = [section]
+        try:
+            repos_to_clone += config.get(section, 'requires').split(',')
+        except NoOptionError:
+            pass
+
+        repos_to_remove = []
+        for to_clone in repos_to_clone:
+            repo = scm.get_repo(to_clone, config, 'clone', development)
+            if repo['branch'] != branch:
+                continue
+            func = repo['function']
+            func(repo['url'], repo['path'], repo['branch'], repo['revision'])
+            repos_to_remove.append(repo['path'])
+        if include_reviews:
+            try:
+                project.fetch_reviews(branch, component=section)
+            except:
+                logger.exception("Exception has occured", exc_info=1)
+                project.create_test_task(TEST_FILE)
+                return
+
+        logger.info('%s Testing...' % name2)
+        test(failfast=fail_fast, dbtype=dbtype, reviews=include_reviews,
+            modules=section, name=name2)
+        for to_remove in repos_to_remove:
+            utils.remove_dir(to_remove, quiet=True)
+
+    os.chdir(old_dir)
+    run("rm -Rf %s" % directory)
+
+
+@task()
+def clean(force=True):
+    scm.prefetch(force=force)
+
+
+def setup(branch='default', force=True, fetch=True):
+    scm.hg_update('config', 'config', force, branch=branch)
+    scm.update(clean=force)
+    if fetch:
+        scm.fetch()
+    scm.unknown(remove=True, quiet=force)
+
+
+TestCollection = Collection()
+TestCollection.add_task(clean)
+TestCollection.add_task(runall)
+TestCollection.add_task(module)
+TestCollection.add_task(modules)
+
