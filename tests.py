2cd5187d4f846932033670733d084e3824ba3f76
?ngel ?lvarez <angel@nan-tic.com>
2017-08-07 17:21:26 +0200

    remove print

--- tests.py
+++ tests.py
@@ -146,7 +146,6 @@ def _module(module, dbtype='sqlite', fail_fast=False, upload=True):
         except hgapi.HgException, e:
             print "Error running %s: %s" % (e.exit_code, str(e))
 
-        print "build:", build[0].test_state
         if build:
             return
 
